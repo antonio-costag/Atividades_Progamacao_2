1ec0456d07ca8b3a882919ee0dd28d8f799b4141
kpnhr <213208550+kpnhr@users.noreply.github.com>
2025-11-24 02:45:18 -0300

    Mudanças estéticas no código

--- lista-duplamente-encadeada.c
+++ lista-duplamente-encadeada.c
@@ -16,7 +16,7 @@ typedef struct estrutura_no_rotulo{
 // a variável global que representa a cabeça (head) da lista
 estrutura_no *cabeca;
 
-int contador_nos(){
+int contador_nos() {
 
     int contador = 0;
     estrutura_no *ponteiro_atual = cabeca;
@@ -30,10 +30,12 @@ int contador_nos(){
 }
 
 // cria o primeiro nó, que serve como cabeça (cabeça é o primeiro elemento da lista)
-void criar_primeiro_no(int valor){
+void criar_primeiro_no(int valor) {
 
-    // modo de se alocar espaço para um novo nó, seguindo a fórmula: tipo_objeto *nome_objeto = (tipo_objeto*) malloc(sizeof(tipo_objeto))
-    // o asterisco (*) aparece na primeira vez para declarar que o objeto é um ponteiro e na segunda vez para fazer o casting de um tipo de ponteiro
+    // modo de se alocar espaço para um novo nó, seguindo a fórmula: 
+    //      tipo_objeto *nome_objeto = (tipo_objeto*) malloc(sizeof(tipo_objeto))
+    // o asterisco (*) aparece na primeira vez para declarar que o objeto é um ponteiro 
+    // e na segunda vez para fazer o casting de um tipo de ponteiro
     cabeca = (estrutura_no*) malloc(sizeof(estrutura_no));
 
     // o nó começa com os endereços anteriores e posteriores nulos, recebendo somente o valor
@@ -43,7 +45,7 @@ void criar_primeiro_no(int valor){
 }
 
 // insere um novo nó no início da lista, que serve como a nova cabeça
-void inserir_inicio(int valor){
+void inserir_inicio(int valor) {
 
     // cria esse novo nó
     estrutura_no *novo_no = (estrutura_no*) malloc(sizeof(estrutura_no));
@@ -57,20 +59,25 @@ void inserir_inicio(int valor){
 }
 
 // insere um novo nó no meio de outros nós, não insere nem no início e nem no final
-void inserir_meio(int valor, int posicao){
+void inserir_meio(int valor, int posicao) {
 
-    // ponteiro descartável que começa com o valor da cabeça. é usado para navegar pela lista sem mudar o valor da cabeça (é usado em várias funções)
+    // ponteiro descartável que começa com o valor da cabeça. é usado para navegar pela
+    // lista sem mudar o valor da cabeça (é usado em várias funções)
     estrutura_no *ponteiro_atual = cabeca;
 
-    // posicao é a variável que determina em qual posicao da lista o novo nó irá ficar; considera-se a primeira posição como 1 ao invés de 0 (classroom que pediu)
-    // se a posição for 1 (inicio do nó) ou menor (nem sequer existe na lista) a função retorna sem inserir nada
+    // posicao é a variável que determina em qual posicao da lista o novo nó irá ficar; 
+    // considera-se a primeira posição como 1 ao invés de 0 (classroom que pediu)
+    // se a posição for 1 (inicio do nó) ou menor (nem sequer existe na lista) a função 
+    // retorna sem inserir nada
     if(posicao <= 1){
         return;
     }
 
-    // a variável posicao vai ser decrementada aqui, o loop é executado enquanto ela for maior que 1 (enquanto ainda não se chegou na posição de inserção)
+    // a variável posicao vai ser decrementada aqui, o loop é executado enquanto ela for
+    // maior que 1 (enquanto ainda não se chegou na posição de inserção)
     while(posicao > 1){
-        // se o ponteiro atual não for a cauda da lista, o loop continua, caso contrário, a função retorna sem inserir nada (ela não insere no final da lista)
+        // se o ponteiro atual não for a cauda da lista, o loop continua, caso contrário, 
+        // a função retorna sem inserir nada (ela não insere no final da lista)
         if(ponteiro_atual->endereco_posterior != NULL){
             ponteiro_atual = ponteiro_atual->endereco_posterior;
             posicao--;
@@ -81,7 +88,8 @@ void inserir_meio(int valor, int posicao){
 
     estrutura_no *novo_no = (estrutura_no*) malloc(sizeof(estrutura_no));
 
-    // é a inserção mais difícil de entender por ser a com mais mudanças nos ponteiros anteriores e posteriores, mas faz um esforço que tu entende (ou faz o L)
+    // é a inserção mais difícil de entender por ser a com mais mudanças nos ponteiros
+    // anteriores e posteriores, mas faz um esforço que tu entende (ou faz o L)
     ponteiro_atual->endereco_anterior->endereco_posterior = novo_no;
     novo_no->endereco_anterior = ponteiro_atual->endereco_anterior;
     novo_no->endereco_posterior = ponteiro_atual;
@@ -90,7 +98,7 @@ void inserir_meio(int valor, int posicao){
 }
 
 // insere um novo nó no final da lista, que serve como cauda (cauda é o último elemento da lista)
-void inserir_final(int valor){
+void inserir_final(int valor) {
 
     estrutura_no *ponteiro_atual = cabeca;
 
@@ -107,7 +115,7 @@ void inserir_final(int valor){
 }
 
 // remove qualquer nó que não seja a cabeça
-void remover_no(int posicao){
+void remover_no(int posicao) {
 
     estrutura_no *ponteiro_atual = cabeca;
 
@@ -130,12 +138,13 @@ void remover_no(int posicao){
         ponteiro_atual->endereco_posterior->endereco_anterior = ponteiro_atual->endereco_anterior;
     }
 
-    // agora que o nó foi removido, free() é chamado para liberar esse espaço de memória que antes estava sendo ocupado (essa função é chamada mais vezes)
+    // agora que o nó foi removido, free() é chamado para liberar esse espaço de memória
+    // que antes estava sendo ocupado (essa função é chamada mais vezes)
     free(ponteiro_atual);
 }
 
 // remove apenas o nó cabeça
-void remover_no_cabeca(){
+void remover_no_cabeca() {
 
     estrutura_no *ponteiro_lixo = cabeca;
 
@@ -145,10 +154,12 @@ void remover_no_cabeca(){
     free(ponteiro_lixo);
 }
 
-// busca e exibe qual o primeiro nó que possui o valor desejado pelo usuário (ou se nenhum nó possui esse valor)
-void buscar(int objetivo){
+// busca e exibe qual o primeiro nó que possui o valor desejado pelo usuário 
+// (ou se nenhum nó possui esse valor)
+void buscar(int objetivo) {
 
-    // contador que começa em 1 para caso o objetivo (valor que queremos encontrar) esteja logo no primeiro nó
+    // contador que começa em 1 para caso o objetivo (valor que queremos encontrar) 
+    // esteja logo no primeiro nó
     int contador = 1;
     estrutura_no *ponteiro_atual = cabeca;
 
@@ -168,21 +179,24 @@ void buscar(int objetivo){
     }
 }
 
-// listamos os nós: número do nó, endereço do nó, endereço anterior ao nó, endereço posterior ao nó e valor do nó
-void listar(){
+// listamos os nós: número do nó, endereço do nó, endereço anterior ao nó, 
+// endereço posterior ao nó e valor do nó
+void listar() {
 
     int contador = 1;
     estrutura_no *ponteiro_atual = cabeca;
     
     while(ponteiro_atual != NULL){
-        printf("-----Nó %d-----\nEndereço: %p\tEnd.Ant: %p\tEnd.Pos: %p\tValor: %d\n\n", contador, ponteiro_atual, ponteiro_atual->endereco_anterior, ponteiro_atual->endereco_posterior, ponteiro_atual->valor);
+        printf("-----Nó %d-----\nEndereço: %p\tEnd.Ant: %p\tEnd.Pos: %p\tValor: %d\n\n", 
+            contador, ponteiro_atual, ponteiro_atual->endereco_anterior, ponteiro_atual->endereco_posterior, ponteiro_atual->valor);
         contador++;
         ponteiro_atual = ponteiro_atual->endereco_posterior;
     }
 }
 
-// função wrapper usada para interagir com o usuário e chamar outras funções que efetivamente fazem coisas (modo simples do usuário interagir com o programa)
-void insercao_no(){
+// Função wrapper usada para interagir com o usuário e chamar outras funções
+// que efetivamente fazem coisas (modo simples do usuário interagir com o programa)
+void insercao_no() {
     
     int contador = contador_nos();
     int valor;
@@ -190,7 +204,8 @@ void insercao_no(){
 
     printf("Insira qual valor você deseja que o novo nó tenha: ");scanf("%d", &valor);
     do {
-        printf("Insira em qual posição da lista você deseja inserir um nó (Insira uma posição que exista na lista ou a posição imediatamente posterior à última. A lista começa por 1): ");scanf("%d", &posicao);
+        printf("Insira em qual posição da lista você deseja inserir um nó (Insira uma posição que exista na lista ou a posição imediatamente posterior à última. A lista começa por 1): ");
+        scanf("%d", &posicao);
     } while(posicao < 1 || posicao > contador+1);
 
     if(posicao == 1){
@@ -203,20 +218,22 @@ void insercao_no(){
 }
 
 // outra função wrapper, mas para remoção de nós ao invés de inserção
-void remocao_no(){
+void remocao_no() {
 
     int contador = contador_nos();
     int posicao;
     int valor_descartavel;
 
     do {
-        printf("Insira a posição do nó que você deseja remover (Insira uma posição que exista na lista. A lista começa por 1): ");scanf("%d", &posicao);
+        printf("Insira a posição do nó que você deseja remover (Insira uma posição que exista na lista. A lista começa por 1): ");
+        scanf("%d", &posicao);
     } while(posicao < 1 || posicao > contador);
 
     if(posicao == 1 && contador > 1){
         remover_no_cabeca();
     } else if(contador == 1){
-        printf("Você está sem nós! Crie um para continuar. Insira seu valor (CTRL+C PARA SAIR): ");scanf("%d", &valor_descartavel);
+        printf("Você está sem nós! Crie um para continuar. Insira seu valor (CTRL+C PARA SAIR): ");
+        scanf("%d", &valor_descartavel);
         free(cabeca);
         criar_primeiro_no(valor_descartavel);
     } else {
@@ -225,23 +242,34 @@ void remocao_no(){
 }
 
 // onde toda a mágica acontece.
-int main(){
-
+int main() {
     int valor_descartavel;
     int resposta = 0;
 
-    printf("Bem-vindo! Crie seu primeiro nó dando um valor inteiro a ele (CTRL+C PARA SAIR): ");scanf("%d", &valor_descartavel);
+    printf("Bem-vindo! Crie seu primeiro nó dando um valor inteiro a ele (CTRL+C PARA SAIR): ");
+    scanf("%d", &valor_descartavel);
     criar_primeiro_no(valor_descartavel);
+    
     printf("Agora com seu primeiro nó, diga o que quer fazer!\n");
 
     // esse loop se repete até que o usuário decida deixar o programa
     while(1){
-        printf("[1] Inserir nó\n[2] Remover nó\n[3] Buscar primeira aparição de um valor em um nó\n[4] Listar nós\n[5] Sair\nResposta: ");scanf("%d", &resposta);
+        printf("[1] Inserir nó\n");
+        printf("[2] Remover nó\n");
+        printf("[3] Buscar primeira aparição de um valor em um nó\n");
+        printf("[4] Listar nós\n");
+        printf("[5] Sair\n");
+        printf("Resposta: ");
+        scanf("%d", &resposta);
         
         switch(resposta){
             case 1: insercao_no(); break;
             case 2: remocao_no(); break;
-            case 3: printf("Insira o valor que você deseja buscar: ");scanf("%d", &valor_descartavel); buscar(valor_descartavel); break;
+            case 3: 
+                printf("Insira o valor que você deseja buscar: ");
+                scanf("%d", &valor_descartavel); 
+                buscar(valor_descartavel); 
+                break;
             case 4: listar(); break;
             case 5: exit(0); break;
             default: break;
